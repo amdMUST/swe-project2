e066194b49fdc44562f27edfd67457825d81fd4c
Ubuntu <ahmed@vm3.juwz52wpcfaurbci24pgdyfaae.bx.internal.cloudapp.net>
2021-11-09 17:51:51 +0000

    Initial commit for the branch that will be working on the Weather api interactions.

--- py_files/weather.py
+++ py_files/weather.py
@@ -1,1 +1,10 @@
 # py file for OpenWeather api
+import requests, json, os
+from dotenv import load_dotenv, find_dotenv
+
+# get the Database URL and change it to postgresql if needed
+load_dotenv(find_dotenv())
+API_KEY = os.environ.get('WEATHER_API_KEY')
+
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+CITY = ''

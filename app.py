6ed3de489f19b0380192aa0b32bbcb6d46f92441
Ubuntu <Mdinh5@Mdinh5.ol52nx5kqemuzfosnmdet2nyva.bx.internal.cloudapp.net>
2021-11-13 22:43:30 +0000

    changed host back to 0.0.0.0

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ def save():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port=int(os.getenv("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))

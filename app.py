a54407b41159403338d5e6af27c5a04fa9ff7c49
mdinh5 <71466348+mdinh5@users.noreply.github.com>
2021-11-13 22:18:47 -0500

    Changed Hostname
    Change the host address

--- app.py
+++ app.py
@@ -209,4 +209,4 @@ def isUserInDB(userID):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port=int(os.getenv("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))

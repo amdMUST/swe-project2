debd600784c463beca093fa9798477c574dd0dfb
Ubuntu <ahmed@vm3.juwz52wpcfaurbci24pgdyfaae.bx.internal.cloudapp.net>
2021-11-24 19:18:13 +0000

    Fixed the issue with the wrapper class not covering the full screen. This concludes the basic styling of the site, Will revist styling at the end to style the 2 new pages we will have and polish the styling everywhere. We should have enough right now to get an idea

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
-# does this work on heroku ?
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 # OAuth 2 client setup

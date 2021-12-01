fc8aec760b98132354c1ba5ce17938bb18fb8fde
Ubuntu <azureuser@VM.zils1nymur1eldwnfqcboyuwze.bx.internal.cloudapp.net>
2021-12-01 01:38:32 +0000

    Fixed database issue with different users saving same city.

--- py_files/models.py
+++ py_files/models.py
@@ -33,4 +33,4 @@ class CityDB(UserMixin, db.Model):
     __tablename__ = "CityDB"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Float)
-    city_name = db.Column(db.String(80), unique=True)
+    city_name = db.Column(db.String(80))
